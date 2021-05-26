aad04d4e4c4e6b9331b201151fbae46366000778
pefonmo <76449619+pefonmo@users.noreply.github.com>
2021-05-26 19:48:12 -0300

    Update and rename formValidations.js to index.js

--- components/RegisterForm/index.js
+++ components/RegisterForm/index.js
@@ -1,17 +1,66 @@
-export const FormValidations = yup.object().shape({
-    credencial: yup
-        .string()
-        .required('*Credencial obrigatória'),
-    password: yup
-        .string()
-        .min(8, 'Minimo 8 caracteres')
-        .max(14, 'Máximo 14 caracteres')
-        .required('*Senha obrigatório'),
-    confirmPassword: yup
-        .string()
-        .min(8, 'Minimo 8 caracteres')
-        .max(14, 'Máximo 14 caracteres')
-        .required('*Confirmação de senha obrigatório'),
-})
+import React, { useState } from 'react';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+import { FormValidations } from './formValidations';
+import useValidation from './hooks/useValidation';
 
-export default FormValidations;
+const initialFormState = {
+    credencial: '',
+    password: '',
+    confirmPassword: ''
+}
+
+const RegisterForm = () => {
+    let formData = [];
+
+    const [form, setForm] = useState(initialFormState);
+    const { errors, invalid } = useValidation(form, FormValidations);
+
+    const setInput = (newValue) => {
+        setForm(form => ({ ...form, ...newValue }));
+    }
+
+    const submitForm = () => {
+        if (localStorage.getItem('formData') != null) {
+            formData = JSON.parse(localStorage.getItem('formData'));
+        }
+        formData.push(form);
+        localStorage.setItem('formData', JSON.stringify(formData));
+        document.getElementById("newRegister").reset();
+    }
+    return (
+        <>
+            <form id="newRegister">
+                <Input
+                    name="credencial"
+                    type="text"
+                    placeholder="Credencial"
+                    onChange={e => setInput({ credencial: e.target.value })}
+                    error={errors.credencial}
+                />
+
+                <Input
+                    name="senha"
+                    type="password"
+                    placeholder="Senha"
+                    onChange={e => setInput({ password: e.target.value })}
+                    error={errors.password}
+                />
+
+                <Input
+                    name="confirmarSenha"
+                    type="password"
+                    placeholder="Confirmar Senha"
+                    onChange={e => setInput({ confirmPassword: e.target.value })}
+                    error={errors.confirmPassword}
+                />
+
+
+                <div className="register--button">
+                    <Button handlerButton={submitForm} disabled={invalid} title="Criar" />
+                </div>
+            </form>
+        </>
+    );
+}
+export default RegisterForm;
