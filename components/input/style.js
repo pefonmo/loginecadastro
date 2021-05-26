5a633d317b5e3729ef2744505ded0cd4c1e2582a
pefonmo <76449619+pefonmo@users.noreply.github.com>
2021-05-26 19:33:58 -0300

    Create style.js

---
+++ components/input/style.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const MyInput = styled.input`
+	width: 250px;
+	height: 40px;
+	border: none;
+	border-radius: 4px;
+	padding: 0 0 0 20px;
+	margin: 10px 0 0 0;
+	color: rgb(54, 53, 53);
+	background-color: #dad8d8;
+`
+export const Span = styled.span`
+	text-align: initial;
+	color: red;
+	padding: 4px 0 2px 2px;
