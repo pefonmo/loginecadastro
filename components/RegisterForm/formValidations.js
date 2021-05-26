8888ecb46d76b8acb41959468b1925cb52fc65f2
pefonmo <76449619+pefonmo@users.noreply.github.com>
2021-05-26 19:49:45 -0300

    Create formValidations.js

---
+++ components/RegisterForm/formValidations.js
@@ -0,0 +1,19 @@
+import * as yup from 'yup';
+
+export const FormValidations = yup.object().shape({
+    credencial: yup
+        .string()
+        .required('*Credencial obrigatório'),
+    password: yup
+        .string()
+        .min(8, 'Minimo 8 caracteres')
+        .max(14, 'Máximo 14 caracteres')
+        .required('*Senha obrigatório'),
+    confirmPassword: yup
+        .string()
+        .min(8, 'Minimo 8 caracteres')
+        .max(14, 'Máximo 14 caracteres')
+        .required('*Confirmação de senha obrigatório'),
+})
+
+export default FormValidations;
